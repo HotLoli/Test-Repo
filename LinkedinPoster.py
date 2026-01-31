e43a3d4af827dc559dc03df2b1230a63a03b95f6
Rajveer <rajveera1424@gmail.com>
2026-01-31 14:37:23 +0530

    Basic functionality works now

--- LinkedinPoster.py
+++ LinkedinPoster.py
@@ -2,10 +2,12 @@ import pathlib, glob, regex as re
 from mutagen import File
 
 def get_duration(lis:list)->dict:
+    print(lis)
+    # exit()
     info_dict={}
     for item in lis:
-        audio=File(item)
-        info_dict[item]=audio.info.length
+        audio=File(str(item[0]))
+        info_dict[item[1]]=(audio.info.length,item[0])
     return info_dict
 
 music_paths = list(pathlib.Path('.').rglob("*.mp3"))
@@ -14,27 +16,32 @@ english_list=[]
 jp=[]
 
 for item in music_paths:
+    print(item, item.name, item)
+    
     english=r"^[\u0000-\u007F]+$"
     english_jp = r'^[\u0000-\u007F\u3040-\u309F\u30A0-\u30FF\u4E00-\u9FFF\uFF00-\uFFEF]+$'
 
-    if re.match(english,item):
-        english_list.append(item)
+    if re.match(english,item.stem):
+        english_list.append([item.absolute(),item.stem])
     
-    elif re.match(english_jp,item):
-        jp.append(item)
+    elif re.match(english_jp,item.stem):
+        jp.append([item.absolute(),item.stem])
 
+print(english_list)
 print(english_list,"\n",jp)
 
 english_duration_dict=get_duration(english_list)
 with open("English.m3u","w") as eng:
     eng.write("#EXTM3U\n")
     for item in english_duration_dict:
-        eng.write(f"#EXTINF:{english_duration_dict[item]}, {item}\n")
+        eng.write(f"#EXTINF:{english_duration_dict[item][0]}, {english_duration_dict[item][1]}\n")
         eng.write(f"{item}\n")
 
-japanese_duration_dict=get_duration(english_jp)
-with open("Japanese.m3u","w") as jp:
+japanese_duration_dict=get_duration(jp)
+with open("Japanese.m3u","w", encoding="UTF-8") as jp:
     jp.write("#EXTM3U\n")
+    print(japanese_duration_dict)
+    # exit()
     for item in japanese_duration_dict:
-        jp.write(f"#EXTINF:{japanese_duration_dict[item]}, {item}\n")
+        jp.write(f"#EXTINF:{japanese_duration_dict[item][0]}, {japanese_duration_dict[item][1]}\n")
         jp.write(f"{item}\n")
