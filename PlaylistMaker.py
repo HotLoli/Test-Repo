4544f164882f9c3f2918f25f6a3853b5a06f99bc
Kaler <rajveer0178.becse25@chitkara.edu.in>
2026-02-18 15:35:24 +0530

    Fixed MAJOR functionality flaws

--- PlaylistMaker.py
+++ PlaylistMaker.py
@@ -4,53 +4,55 @@ from mutagen import File
 def get_duration(lis:list)->dict:
     info_dict={}
     for item in lis:
-        audio=File(str(item[0]))
-        info_dict[item[1]]=(audio.info.length,item[0])
+        audio=File(str(item[0])) #Extracts all data from  file
+        info_dict[item[1]]=(audio.info.length,item[0]) #Adds duration and path tuple to Title key in info_dict
     return info_dict
 
-def language_detection(file_paths:list):
+#This function divides the songs into sub lists based on language
+def language_detection(file_paths:list)->list:
 
+    english=r"^[\u0000-\u007F]+$" #Regex pattern for english
+    english_jp = r'^[\u0000-\u007F\u3040-\u309F\u30A0-\u30FF\u4E00-\u9FFF\uFF00-\uFFEF]+$' #Regex for JP
+    english_list=[]
+    jp_list=[]
+    unclassified=[]
     for item in file_paths:
         
-        english=r"^[\u0000-\u007F]+$"
-        english_jp = r'^[\u0000-\u007F\u3040-\u309F\u30A0-\u30FF\u4E00-\u9FFF\uFF00-\uFFEF]+$'
-        english_list=[]
-        jp_list=[]
-        if re.match(english,item.stem):
+        if re.match(english,item.stem): #If name regex pattern matches english regex pattern
             english_list.append([item.absolute(),item.stem])
+            
         
         elif re.match(english_jp,item.stem):
             jp_list.append([item.absolute(),item.stem])
         
-        return english_list, jp_list
+        else:
+            unclassified.append(item)
+    
+    return english_list, jp_list, unclassified
 
 print(f"{"PlaylistMaker by KalerKaler":*^40}")
 
 extensions=(".mp3",".flac",".m4a",".ogg")
-music_paths = [_ for _ in pathlib.Path(".").rglob("*") if _.suffix.lower() in extensions]
+music_paths = [_ for _ in pathlib.Path(".").rglob("*") if _.suffix.lower() in extensions] #Finds files with the pre-defined extentions
 if not music_paths:
     print("No files found! Make sure music files exist in the same directory.")
     exit()
-english_list,jp_list=language_detection(music_paths)
-
+    
+english_list, jp_list, unknown=language_detection(music_paths)
 
 print(f"Found {len(english_list)} English songs.")
 print(f"Found {len(jp_list)} Japanese songs.")
+print(f"Found {len(unknown)} songs in an unknown language.")
 
-if len(english_list):
-    english_duration_dict=get_duration(english_list)
-    english_playlist_name=input("Enter the name for the English playlist file\n")
-    with open(english_playlist_name+".m3u","w") as eng:
-        eng.write("#EXTM3U\n")
-        for item in english_duration_dict:
-            eng.write(f"#EXTINF:{english_duration_dict[item][0]}, {english_duration_dict[item][1]}\n")
-            eng.write(f"{item}\n")
+lists=(english_list, jp_list)
 
-if len(jp_list):
-    japanese_duration_dict=get_duration(jp_list)
-    japanese_playlist_name=input("Enter the name for the Japanese playlist file\n")
-    with open(japanese_playlist_name+".m3u","w",encoding="UTF-8") as jp:
-        jp.write("#EXTM3U\n")
-        for item in japanese_duration_dict:
-            jp.write(f"#EXTINF:{japanese_duration_dict[item][0]}, {japanese_duration_dict[item][1]}\n")
-            jp.write(f"{item}\n")
+for index, item in enumerate(lists):
+    if item:
+        duration_dict=get_duration(item)
+        playlist_name=input(f"Enter the name for {"Japanese" if index else "English"} playlist\n")
+        with open(playlist_name+".m3u","w", encoding="UTF-8") as plt:
+            plt.write("#EXTM3U\n")
+            for itm in duration_dict:
+                plt.write(f"#EXTINF:{duration_dict[itm][0]}, {duration_dict[itm][1]}\n")
+            
+print("Done! Have fun listening :)")
